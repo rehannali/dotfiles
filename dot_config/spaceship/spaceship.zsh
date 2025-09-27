74f9628cf294b4cc1cc88527a5d9c17e849ffe27
Rehan Ali <rehannali@programmer.net>
2025-09-27 21:36:01 +0500

    add spaceship

---
+++ dot_config/spaceship/spaceship.zsh
@@ -0,0 +1,189 @@
+spaceship add ember
+spaceship add --before char vi_mode
+
+SPACESHIP_PROMPT_ORDER=(
+  time           # Time stamps section
+  user           # Username section
+  dir            # Current directory section
+  host           # Hostname section
+  git            # Git section (git_branch + git_status)
+  hg             # Mercurial section (hg_branch  + hg_status)
+  package        # Package version
+  node           # Node.js section
+  bun            # Bun section
+  deno           # Deno section
+  ruby           # Ruby section
+  python         # Python section
+  elm            # Elm section
+  elixir         # Elixir section
+  xcode          # Xcode section
+  swift          # Swift section
+  golang         # Go section
+  perl           # Perl section
+  php            # PHP section
+  rust           # Rust section
+  haskell        # Haskell Stack section
+  scala          # Scala section
+  kotlin         # Kotlin section
+  java           # Java section
+  lua            # Lua section
+  dart           # Dart section
+  julia          # Julia section
+  crystal        # Crystal section
+  docker         # Docker section
+  docker_compose # Docker section
+  aws            # Amazon Web Services section
+  gcloud         # Google Cloud Platform section
+  azure          # Azure section
+  venv           # virtualenv section
+  conda          # conda virtualenv section
+  dotnet         # .NET section
+  ocaml          # OCaml section
+  vlang          # V section
+  zig            # Zig section
+  purescript     # PureScript section
+  erlang         # Erlang section
+  kubectl        # Kubectl context section
+  ansible        # Ansible section
+  terraform      # Terraform workspace section
+  pulumi         # Pulumi stack section
+  ibmcloud       # IBM Cloud section
+  nix_shell      # Nix shell
+  gnu_screen     # GNU Screen section
+  exec_time      # Execution time
+  async          # Async jobs indicator
+  line_sep       # Line break
+  battery        # Battery level and status
+  jobs           # Background jobs indicator
+  exit_code      # Exit code section
+  sudo           # Sudo indicator
+  char           # Prompt character
+)
+
+
+  # PROMPT
+   SPACESHIP_CHAR_SYMBOL="➜  "
+   SPACESHIP_PROMPT_ADD_NEWLINE=true
+   SPACESHIP_PROMPT_SEPARATE_LINE=true
+   SPACESHIP_PROMPT_PREFIXES_SHOW=true
+   SPACESHIP_PROMPT_SUFFIXES_SHOW=true
+   SPACESHIP_PROMPT_DEFAULT_PREFIX=" "
+   SPACESHIP_PROMPT_DEFAULT_SUFFIX=" "
+   # TIME
+   SPACESHIP_TIME_SHOW=false
+   SPACESHIP_TIME_PREFIX="at "
+   SPACESHIP_TIME_SUFFIX="$SPACESHIP_PROMPT_DEFAULT_SUFFIX"
+   #SPACESHIP_TIME_FORMAT='%D{%H:%M:%S.%.}'
+   SPACESHIP_TIME_12HR=false
+   SPACESHIP_TIME_COLOR="yellow"
+   # USER
+   SPACESHIP_USER_SHOW=true
+   SPACESHIP_USER_PREFIX="with "
+   SPACESHIP_USER_SUFFIX="$SPACESHIP_PROMPT_DEFAULT_SUFFIX"
+   SPACESHIP_USER_COLOR="yellow"
+   SPACESHIP_USER_COLOR_ROOT="red"
+   # HOST
+   SPACESHIP_HOST_SHOW=true
+   SPACESHIP_HOST_PREFIX="at "
+   SPACESHIP_HOST_SUFFIX="$SPACESHIP_PROMPT_DEFAULT_SUFFIX"
+   SPACESHIP_HOST_COLOR="green"
+   # DIR
+   SPACESHIP_DIR_SHOW=true
+   SPACESHIP_DIR_PREFIX="in "
+   SPACESHIP_DIR_SUFFIX="$SPACESHIP_PROMPT_DEFAULT_SUFFIX"
+   SPACESHIP_DIR_TRUNC=3
+   SPACESHIP_DIR_COLOR="cyan"
+   # GIT
+   SPACESHIP_GIT_SHOW=true
+   SPACESHIP_GIT_PREFIX="on "
+   SPACESHIP_GIT_SUFFIX="$SPACESHIP_PROMPT_DEFAULT_SUFFIX"
+   SPACESHIP_GIT_SYMBOL=" "
+   # GIT BRANCH
+   SPACESHIP_GIT_BRANCH_SHOW=true
+   SPACESHIP_GIT_BRANCH_PREFIX="$SPACESHIP_GIT_SYMBOL"
+   SPACESHIP_GIT_BRANCH_SUFFIX=""
+   SPACESHIP_GIT_BRANCH_COLOR="magenta"
+   # GIT STATUS
+   SPACESHIP_GIT_STATUS_SHOW=true
+   SPACESHIP_GIT_STATUS_PREFIX=" ["
+   SPACESHIP_GIT_STATUS_SUFFIX="]"
+   SPACESHIP_GIT_STATUS_COLOR="red"
+   SPACESHIP_GIT_STATUS_UNTRACKED="?"
+   SPACESHIP_GIT_STATUS_ADDED="+"
+   SPACESHIP_GIT_STATUS_MODIFIED="!"
+   SPACESHIP_GIT_STATUS_RENAMED="»"
+   SPACESHIP_GIT_STATUS_DELETED="✘"
+   SPACESHIP_GIT_STATUS_STASHED="$"
+   SPACESHIP_GIT_STATUS_UNMERGED="="
+   SPACESHIP_GIT_STATUS_AHEAD="⇡"
+   SPACESHIP_GIT_STATUS_BEHIND="⇣"
+   SPACESHIP_GIT_STATUS_DIVERGED="⇕"
+   # NODE
+   SPACESHIP_NODE_SHOW=true
+   SPACESHIP_NODE_PREFIX="$SPACESHIP_PROMPT_DEFAULT_PREFIX"
+   SPACESHIP_NODE_SUFFIX="$SPACESHIP_PROMPT_DEFAULT_SUFFIX"
+   SPACESHIP_NODE_SYMBOL="⬢ "
+   SPACESHIP_NODE_DEFAULT_VERSION=""
+   SPACESHIP_NODE_COLOR="green"
+   # RUBY
+   SPACESHIP_RUBY_SHOW=true
+   SPACESHIP_RUBY_PREFIX="$SPACESHIP_PROMPT_DEFAULT_PREFIX"
+   SPACESHIP_RUBY_SUFFIX="$SPACESHIP_PROMPT_DEFAULT_SUFFIX"
+   SPACESHIP_RUBY_SYMBOL="💎 "
+   SPACESHIP_RUBY_COLOR="red"
+   # XCODE
+   SPACESHIP_XCODE_SHOW_LOCAL=true
+   SPACESHIP_XCODE_SHOW_GLOBAL=false
+   SPACESHIP_XCODE_PREFIX="$SPACESHIP_PROMPT_DEFAULT_PREFIX"
+   SPACESHIP_XCODE_SUFFIX="$SPACESHIP_PROMPT_DEFAULT_SUFFIX"
+   SPACESHIP_XCODE_SYMBOL="🛠 "
+   SPACESHIP_XCODE_COLOR="blue"
+   # SWIFT
+   SPACESHIP_SWIFT_SHOW_LOCAL=true
+   SPACESHIP_SWIFT_SHOW_GLOBAL=false
+   SPACESHIP_SWIFT_PREFIX="$SPACESHIP_PROMPT_DEFAULT_PREFIX"
+   SPACESHIP_SWIFT_SUFFIX="$SPACESHIP_PROMPT_DEFAULT_SUFFIX"
+   SPACESHIP_SWIFT_SYMBOL="🐦 "
+   SPACESHIP_SWIFT_COLOR="yellow"
+   # GOLANG
+   SPACESHIP_GOLANG_SHOW=true
+   SPACESHIP_GOLANG_PREFIX="$SPACESHIP_PROMPT_DEFAULT_PREFIX"
+   SPACESHIP_GOLANG_SUFFIX="$SPACESHIP_PROMPT_DEFAULT_SUFFIX"
+   SPACESHIP_GOLANG_SYMBOL="🐹 "
+   SPACESHIP_GOLANG_COLOR="cyan"
+   # PHP
+   SPACESHIP_PHP_SHOW=true
+   SPACESHIP_PHP_PREFIX="$SPACESHIP_PROMPT_DEFAULT_PREFIX"
+   SPACESHIP_PHP_SUFFIX="$SPACESHIP_PROMPT_DEFAULT_SUFFIX"
+   SPACESHIP_PHP_SYMBOL="🐘 "
+   SPACEHIP_PHP_COLOR="blue"
+   # RUST
+   SPACESHIP_RUST_SHOW=true
+   SPACESHIP_RUST_PREFIX="$SPACESHIP_PROMPT_DEFAULT_PREFIX"
+   SPACESHIP_RUST_SUFFIX="$SPACESHIP_PROMPT_DEFAULT_SUFFIX"
+   SPACESHIP_RUST_SYMBOL="𝗥 "
+   SPACESHIP_RUST_COLOR="red"
+   # DOCKER
+   SPACESHIP_DOCKER_SHOW=true
+   SPACESHIP_DOCKER_PREFIX="on "
+   SPACESHIP_DOCKER_SUFFIX="$SPACESHIP_PROMPT_DEFAULT_SUFFIX"
+   SPACESHIP_DOCKER_SYMBOL="🐳 "
+   SPACESHIP_DOCKER_COLOR="cyan"
+   # VENV
+   SPACESHIP_VENV_SHOW=true
+   SPACESHIP_VENV_PREFIX="$SPACESHIP_PROMPT_DEFAULT_PREFIX"
+   SPACESHIP_VENV_SUFFIX="$SPACESHIP_PROMPT_DEFAULT_SUFFIX"
+   SPACESHIP_VENV_COLOR="blue"
+   # PYENV
+   SPACESHIP_PYTHON_SHOW=true
+   SPACESHIP_PYTHON_PREFIX="$SPACESHIP_PROMPT_DEFAULT_PREFIX"
+   SPACESHIP_PYTHON_SUFFIX="$SPACESHIP_PROMPT_DEFAULT_SUFFIX"
+   SPACESHIP_PYTHON_SYMBOL="🐍 "
+   SPACESHIP_PYTHON_COLOR="yellow"
+   # VI_MODE
+   SPACESHIP_VI_MODE_SHOW=true
+   SPACESHIP_VI_MODE_PREFIX=""
+   SPACESHIP_VI_MODE_SUFFIX="$SPACESHIP_PROMPT_DEFAULT_SUFFIX"
+   SPACESHIP_VI_MODE_INSERT="[I]"
+   SPACESHIP_VI_MODE_NORMAL="[N]"
+   SPACESHIP_VI_MODE_COLOR="white"
